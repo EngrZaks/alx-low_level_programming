78f5d7328d96fd4b8c10e2635fc21742dc292416
EngrZaks <abzakariyya@gmail.com>
2023-03-24 07:06:53 -0700

    changed output of 101 to return 59 characters

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,6 +11,6 @@
 int main(void)
 {
 const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(STDERR_FILENO, message, 37);
+write(STDERR_FILENO, message, 59);
 return (0);
 }
