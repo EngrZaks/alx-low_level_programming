c44da75d02cf0db2a119f0e8812606d90612bf36
EngrZaks <abzakariyya@gmail.com>
2023-03-24 04:51:33 -0700

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a message without printf or puts
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, message, 37);
+return (0);
+}
