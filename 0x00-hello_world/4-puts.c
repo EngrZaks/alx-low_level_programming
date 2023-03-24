c309a68055ce5fef1ec3646675e38109b86f6614
EngrZaks <abzakariyya@gmail.com>
2023-03-23 17:19:27 -0700

    removed space to pass betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-*
-* Description: This function prints a message to the console.
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Description: Prints a message to the console
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+printf("\"Programming is like building a multilingual puzzle");
+return (0);
 }
