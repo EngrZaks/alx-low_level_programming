b0fe263f24399ea7821ba750f4deae218e925b82
EngrZaks <abzakariyya@gmail.com>
2023-03-24 07:28:37 -0700

    splitted the string into tow lines

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,9 @@
 
 int main(void)
 {
-const char *message = "and that piece of art is useful\"
-- Dora Korpar, 2015-10-19\n";
-write(STDERR_FILENO, message, 59);
+const char *message_part1 = "and that piece of art is useful\" - ";
+const char *message_part2 = "Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, message_part1, 35);
+write(STDERR_FILENO, message_part2, 24);
 return (1);
 }
